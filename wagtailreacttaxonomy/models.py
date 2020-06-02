4035229cfdb6cc11ea519c250c8d9e78df1d152c
David Crees <david.a.crees@gmail.com>
2020-06-02 01:16:34 +0100

    Output vocab info to blob json

--- wagtailreacttaxonomy/models.py
+++ wagtailreacttaxonomy/models.py
@@ -20,13 +20,16 @@ class TaxonomyTerms(models.Model):
 
 @receiver(post_save, sender=TaxonomyTerms)
 def update_taxonomy_terms_on_blobstore(sender, instance, **kwargs):
-    data = json.loads(instance.terms_json)
-    terms_with_vocab = get_terms_from_terms_json(data)
-    terms_with_vocab_json = to_json(terms_with_vocab)
-
     try:
+        data = json.loads(instance.terms_json)
+        terms_with_vocab = get_terms_from_terms_json(data)
+        vocabs = get_vocabs_from_terms_json(data)
+        json = dict()
+        json['vocabs'] = vocabs
+        json['terms'] = terms_with_vocab
+
         blob_service = BlockBlobService(account_name=settings.AZURE_ACCOUNT_NAME, account_key=settings.AZURE_ACCOUNT_KEY)
-        blob_service.create_blob_from_text(settings.AZURE_CONTAINER, f'taxonomy/{instance.taxonomy_id}.json', terms_with_vocab_json)
+        blob_service.create_blob_from_text(settings.AZURE_CONTAINER, f'taxonomy/{instance.taxonomy_id}.json', to_json(json))
     except Exception as e:
         logger.info('Could not build taxonomy json and send to BlobStore %s', e)
 
@@ -53,6 +56,16 @@ def get_terms_from_children(children, vocab_code, level):
                 child_terms = get_terms_from_children(children, vocab_code, next_level)
                 terms.update(child_term)
     return terms
+
+def get_vocabs_from_terms_json(data):
+    vocabs = dict()
+    level = 1
+    for obj in data:
+        if obj.get('type') == 'vocabulary':
+            vocab_code = obj.get('code')
+            vocab_label = obj.get('label')
+            vocabs[vocab_code] = vocab_label
+    return vocabs
 
 def to_json(data):
     return json.dumps(data)
