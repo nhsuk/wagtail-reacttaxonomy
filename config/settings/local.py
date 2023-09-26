11ea77d394a6f8dca0ca8e786816d51779234e51
Ross Clark <ross_clark@outlook.com>
2023-09-26 16:04:10 +0100

    add debugger to local env

--- config/settings/local.py
+++ config/settings/local.py
@@ -30,6 +30,11 @@ LOGGING = {
     },
 }
 
+if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+        import debugpy
+        debugpy.listen(("0.0.0.0", 3000))
+        print('Attached!')
+
 try:
     from .local import *
 except ImportError:
